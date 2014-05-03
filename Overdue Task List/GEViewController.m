110d49a1886860d5b1a5fe22ecc21f5d846a63fe
Gary Edgcombe <garyedgcombe@garys-mbp.lan>
2014-05-03 12:46:18 +0100

    Initial Commit

---
+++ Overdue Task List/GEViewController.m
@@ -0,0 +1,29 @@
+//
+//  GEViewController.m
+//  Overdue Task List
+//
+//  Created by Gary Edgcombe on 03/05/2014.
+//  Copyright (c) 2014 Gary Edgcombe Code. All rights reserved.
+//
+
+#import "GEViewController.h"
+
+@interface GEViewController ()
+
+@end
+
+@implementation GEViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Overdue Task List/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Overdue Task List
+//
+//  Created by Gary Edgcombe on 03/05/2014.
+//  Copyright (c) 2014 Gary Edgcombe Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GEAppDelegate class]));
+    }
+}
